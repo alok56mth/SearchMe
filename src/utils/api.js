7d3224c16c599becb924f1bbc2b0b6000d708227
Alok Kumar <alok56mth@gmail.com>
2025-07-30 20:38:36 +0530

    google search

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,35 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = "https://www.googleapis.com/customsearch/v1"
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 
 const params = {
-    key: 'AIzaSyDDe_D81bmARRYhTMZICYXofSHwWLlHewE',
-    cx: '50e7fd036be294cb2'
-}
-
-export const fetchDataFromApi = async (payload) => {
-    const {data} = await axios.get(BASE_URL, {
-        params: {...params, ...payload}
-    })
-    return data;
+  key: import.meta.env.VITE_GOOGLE_API_KEY,
+  cx: import.meta.env.VITE_GOOGLE_CX,
 };
 
-// export const fetchDataFromApi = async (payload) => {
-//   try {
-//     const { data } = await axios.get(BASE_URL, {
-//       params: { ...params, ...payload }
-//     });
-//     return data;
-//   } catch (error) {
-//     if (error.response?.status === 429) {
-//       console.warn("Rate limit exceeded. Retrying in 5 seconds...");
-//       await new Promise(res => setTimeout(res, 5000));
-//       return await fetchDataFromApi(payload); // retry once
-//     } else {
-//       console.error("API error:", error.message);
-//       throw error;
-//     }
-//   }
-// };
-
-
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};
